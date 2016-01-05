8468f22ce35a3a72a973f7f644c75e28d726b9a2
derekchen <derek_chen@trend.com.tw>
2016-01-05 18:34:51 +0800

    buy episode 1, price is 100.

---
+++ Day2/PotterShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace PotterShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        private Dictionary<int, int> cartItems;
+
+        public PotterShoppingCart()
+        {
+            cartItems = new Dictionary<int, int>();
+        }
+
+        public int getTotalAmount()
+        {
+            int total = 0;
+            foreach (KeyValuePair<int, int> item in cartItems)
+            {
+                if (item.Value == 1) { total += 100; }
+            }
+
+            return total;
+        }
+
+        public void addToCart(int bookId)
+        {
+            if (cartItems.ContainsKey(bookId)) { 
+                cartItems[bookId] += 1;
+            }
+            else
+            {
+                cartItems.Add(bookId, 1);
+            }
+        }
+    }
+}
---
+++ Day2/PotterShoppingCartTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PotterShoppingCart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod()]
+        public void getTotalAmountTest_第一集買了一本_其他都沒買_價格應為100元()
+        {
+            var target = new PotterShoppingCart();
+            target.addToCart(1);
+            var actual = target.getTotalAmount();
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
